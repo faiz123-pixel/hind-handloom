a5411905fe7a30e604dc766a930f73d90204d4e1
faiz123-pixel <fa123izansari@gmail.com>
2024-10-13 00:51:35 +0530

    heroku

--- hindhandloom/settings.py
+++ hindhandloom/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 import os
 
 
-import django_heroku
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,8 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 STATIC_URL = 'static/'
 
