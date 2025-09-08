d8b4dbff611aa2c040a7ca4e0d370580f8112d05
faiz123-pixel <fa123izansari@gmail.com>
2025-09-09 00:04:39 +0530

    Updated Hindhandloom

--- hindhandloom/settings.py
+++ hindhandloom/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o=t3k+q*ka@rj(ee5%7smgl_--d-a==xki@(2llt!0wg6#g=m&'
-
+# SECRET_KEY = 'django-insecure-o=t3k+q*ka@rj(ee5%7smgl_--d-a==xki@(2llt!0wg6#g=m&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'c51d650f9e86d8cd94f8690edbe8f95f')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['www.hind-handloom.com']
+ALLOWED_HOSTS = ['hind-handloom.onrender.com']
 
 
 # Application definition
